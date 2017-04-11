129cfab8b4679ead5c23493938d9419198b0824f
yongseoklee <yongseokleecom@gmail.com>
2017-04-12 00:31:03 +0900

    엑셀데이터 적재를 위한 번들클래스 추가

--- src/main/java/kr/qusi/spring/servlet/view/excel/Excel2View.java
+++ src/main/java/kr/qusi/spring/servlet/view/excel/Excel2View.java
@@ -6,7 +6,7 @@ import org.springframework.web.servlet.view.AbstractUrlBasedView;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.*;
-import java.util.Map;
+import java.util.*;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
@@ -20,6 +20,7 @@ public class Excel2View extends AbstractUrlBasedView {
     // ========================================
     // ZIP
     // ========================================
+
     /**
      * ZIP 압축
      *
@@ -85,6 +86,149 @@ public class Excel2View extends AbstractUrlBasedView {
         } finally {
             IOUtils.closeQuietly(is);
         }
+    }
+
+    // ========================================
+    // Bundle - 엑셀 적재정보
+    // ========================================
+
+    public static class Bundle {
+
+        /** 분할 크기 최댓값 (xls 최대행 65k 또는 성능상 이유로 제한) */
+        private static final int MAX_SPLIT_SIZE = 60000;
+
+        /** 분할 크기 기본값 */
+        private static final int DEFAULT_SPLIT_SIZE = 10000;
+
+        /** 저장될 파일명 */
+        private String filename;
+
+        /** 템플릿 명 */
+        private String template;
+
+        /** 분할 크기 (기본값: 10000) */
+        private int splitSize = DEFAULT_SPLIT_SIZE;
+
+        /** 적재 데이터 */
+        private Map<String, Object> extras;
+
+        public String getFilename() {
+            return filename;
+        }
+
+        public void setFilename(String filename) {
+            this.filename = filename;
+        }
+
+        public String getTemplate() {
+            return template;
+        }
+
+        public void setTemplate(String template) {
+            this.template = template;
+        }
+
+        public int getSplitSize() {
+            return splitSize;
+        }
+
+        public void setSplitSize(int splitSize) {
+            if (MAX_SPLIT_SIZE < splitSize)
+                throw new IllegalArgumentException("The maximum value of 'splitSize' is " + MAX_SPLIT_SIZE);
+
+            this.splitSize = splitSize;
+        }
+
+        public void putExtra(String key, Object value) {
+            if (extras == null)
+                extras = new HashMap<>();
+
+            // Map 은 입력순서를 유지하기위해 LinkedHashMap 처리
+            if (value instanceof Map) {
+                extras.put(key, new LinkedHashMap((Map) value));
+            } else {
+                extras.put(key, value);
+            }
+        }
+
+        public Object getExtra(String key) {
+            return extras != null ? extras.get(key) : null;
+        }
+
+        public Map<String, Object> getExtras() {
+            return extras == null ? null : Collections.unmodifiableMap(extras);
+        }
+
+        public List<Map<String, Object>> getExtrasAsList() {
+            if (extras == null)
+                return null;
+
+            int maxSize = getMaxSize();
+            int count = (int) Math.ceil((double) maxSize / (double) getSplitSize());
+            List<Map<String, Object>> list = new ArrayList<>();
+
+            for (int i = 0; i < count; i++) {
+                int startIdx = i * getSplitSize();
+                int endIdx = (i + 1) * getSplitSize();
+
+                Map<String, Object> extra = new HashMap<>();
+
+                for (String extraKey : extras.keySet()) {
+                    Object oObj = extras.get(extraKey);
+                    Object nObj = oObj;
+
+                    if (oObj instanceof List) {
+                        List oList = ((List) oObj);
+
+                        if (startIdx < oList.size()) {
+                            nObj = oList.subList(startIdx, Math.min(endIdx, oList.size()));
+                        } else {
+                            nObj = Collections.EMPTY_LIST;
+                        }
+                    } else if (oObj instanceof Map) {
+                        Map oMap = ((Map) oObj);
+
+                        if (startIdx < oMap.size()) {
+                            List<String> oKeys = new ArrayList<String>(oMap.keySet())
+                                    .subList(startIdx, Math.min(endIdx, oMap.size()));
+
+                            nObj = new HashMap<String, Object>();
+                            for (String oKey : oKeys) {
+                                ((Map) nObj).put(oKey, oMap.get(oKey));
+                            }
+                        } else {
+                            nObj = Collections.EMPTY_MAP;
+                        }
+                    }
+
+                    extra.put(extraKey, nObj);
+                }
+
+                list.add(extra);
+            }
+
+            return list;
+        }
+
+        /**
+         * 적재된 데이터 Map, List 형식에서 최대크기 반환
+         */
+        private int getMaxSize() {
+            if (extras == null)
+                return 0;
+
+            int maxSize = 0;
+            for (Object o : extras.values()) {
+                if (o instanceof List) {
+                    maxSize = Math.max(maxSize, ((List) o).size());
+                } else if (o instanceof Map) {
+                    maxSize = Math.max(maxSize, ((Map) o).size());
+                }
+            }
+
+            return maxSize;
+        }
+
     }
 
 }
